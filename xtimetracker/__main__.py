5ee87d5c65b1b6e11cf9cb7fa4477dd7d3fca850
David Alfonso <developer@davidalfonso.es>
2020-12-30 14:16:10 +0100

    Support running as a module
    https://docs.python.org/3.6/library/__main__.html

---
+++ xtimetracker/__main__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2020 David Alfonso
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .cli import cli
+
+
+if __name__ == "__main__":
+    # run the same function as in the console entry point
+    cli()
